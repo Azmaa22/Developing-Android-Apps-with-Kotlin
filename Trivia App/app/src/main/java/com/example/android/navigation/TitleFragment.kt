69c2eafe331a7f407206127d557a36cdee93dcd2
akhaled <akhaled@cairo.aas.com.sa>
2023-04-09 17:55:10 +0200

    used fragment directions instead of ids

--- Trivia App/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia App/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -20,8 +20,12 @@ class TitleFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
       // binding.playButton.setOnClickListener { view: View ->view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)  }
 
-        binding.playButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.playButton.setOnClickListener { view: View ->
+
+            view.findNavController()
+                .navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+
         setHasOptionsMenu(true)
 
         return binding.root    }
