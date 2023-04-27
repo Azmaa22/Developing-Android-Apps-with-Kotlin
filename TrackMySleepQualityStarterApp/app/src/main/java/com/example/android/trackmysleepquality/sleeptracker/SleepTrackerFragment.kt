d79fe111ddedd50c0c89f0dcf56396c2ddf37acd
akhaled <akhaled@cairo.aas.com.sa>
2023-04-27 07:01:17 +0200

    add sleep tracker app with all features

--- TrackMySleepQualityStarterApp/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ TrackMySleepQualityStarterApp/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -16,14 +16,22 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A fragment with buttons to record start and end times for sleep, which are saved in
@@ -37,20 +45,50 @@ class SleepTrackerFragment : Fragment() {
      *
      * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         // Get a reference to the binding object and inflate the fragment views.
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_sleep_tracker, container, false)
+            inflater, R.layout.fragment_sleep_tracker, container, false
+        )
 
-        //TODO (01) Update onCreateView() to get an instance of SleepTrackerViewModel
+        val application: Application = requireNotNull(this.activity).application;
+
+        val dataSource: SleepDatabaseDao = SleepDatabase.getInstance(application).sleepDatabaseDao;
+
         //using the factory.
+        val sleepTrackerViewModelFactory: SleepTrackerViewModelFactory =
+            SleepTrackerViewModelFactory(dataSource, application);
 
-        //TODO (02) Update to set this as the lifecycle owner of the binding.
+        val sleepTrackerViewModel: SleepTrackerViewModel =
+            ViewModelProviders.of(this, sleepTrackerViewModelFactory)
+                .get(SleepTrackerViewModel::class.java);
 
-        //TODO (04) Update to assign sleepTrackerViewModel binding variable
+        binding.setLifecycleOwner(this)
         //to the sleepTrackerViewModel.
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+        sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner, Observer { night ->
+            night?.let {
+                this.findNavController().navigate(
+                    SleepTrackerFragmentDirections
+                        .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId)
+                )
+                sleepTrackerViewModel.doneNavigating()
+            }
+        })
+        sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
+            if (it == true) { // Observed state is true.
+                Snackbar.make(
+                    activity!!.findViewById(android.R.id.content),
+                    getString(R.string.cleared_message),
+                    Snackbar.LENGTH_SHORT // How long to display the message.
+                ).show()
+                sleepTrackerViewModel.doneShowingSnackbar()
+            }
+        })
 
         return binding.root
     }
